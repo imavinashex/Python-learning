bc8b596a6f7d164abf0da3af2b6f250666b8369f
imavinashex <avinashrf99-git@gmail.com>
2025-11-28 16:23:58 +0530

    Solved by imavinashex

--- Solving_py_questions/06_question.py
+++ Solving_py_questions/06_question.py
@@ -9,3 +9,21 @@ print("sum:", int(num1) + int(num2))
 print("difference:", int(num1) - int(num2))
 print("product:", int(num1) * int(num2))
 print("quotient:", int(num1) / int(num2))
+
+
+#other way to do this 
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print("a + b=", a + b)
+print("a - b=", a - b)
+print("a * b=", a * b)
+print("a / b=", a / b)
+print("a ** b=", a ** b)
+
+
+
+# Second way look sooo clean is essy to understand  :) 
+#  Great learnig for me :) improving my skills  :)  @imavinashex IG.
+
