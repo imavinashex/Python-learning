5ed8f8cbf2409761f6ad254af104c21f34acce1f
imavinashex <avinashrf99-git@gmail.com>
2025-11-28 11:33:29 +0530

    Add all project file

--- Day_2/Operaters/tempCodeRunnerFile.py
+++ Day_2/Operaters/tempCodeRunnerFile.py
@@ -8,3 +8,11 @@ elif age == 18:
     print("You are just become Adult")
 else:
     print("You have responsibility")
+
+
+
+
+    # this is a testing of github push and learning git commands
+# hi my friend
+print("This is an easy 3-line code.")
+print("It demonstrates how to print messages.")
