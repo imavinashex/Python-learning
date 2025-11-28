dabab75e004741fd2de1d38246fc9fa560d777e0
imavinashex <avinashrf99-git@gmail.com>
2025-11-28 11:06:06 +0530

    update testing.py

--- Python exercises/testing.py
+++ Python exercises/testing.py
@@ -1,16 +1,5 @@
 # hi my friend
 print("This is an easy 3-line code.")
 print("It demonstrates how to print messages.")
-print("Hope you find it helpful!")
-# hi = "hello" my friend how are you? p
-# hi my friend
-print("This is an easy 3-line code.")
-print("It demonstrates how to print messages.")
-print("Hope you find it helpful!")
-# hi = "hello" my friend how are you? p
-# hi my friend
-print("This is an easy 3-line code.")
-print("It demonstrates how to print messages.")
-print("Hope you find it helpful!")
-# hi = "hello" my friend how are you? p
+
 
