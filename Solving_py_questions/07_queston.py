b8c9fb14f98796c59b3819876972ab6efd65ae94
imavinashex <avinashrf99-git@gmail.com>
2025-11-29 13:55:08 +0530

    update all file

--- Solving_py_questions/07_queston.py
+++
@@ -1,7 +0,0 @@
-# 7th question:- Escape Sequences
-# Harry said, "Python is awesome!"
-# This is on a new line.
-# This is a tab -> <- here
-# ans= 
-
-print("Harry said, \"Python is awesome!\"\nThis is on a new line.\nThis is a tab ->\t<- here")
