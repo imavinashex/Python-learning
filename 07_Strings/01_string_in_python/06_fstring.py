027a1b6e8cae9b7466f7705ad3f1b9067568818f
imavinashex <avinashrf99-git@gmail.com>
2025-12-13 15:58:49 +0530

    F str learned

---
+++ 07_Strings/01_string_in_python/06_fstring.py
@@ -0,0 +1,14 @@
+"""
+Docstring for 01_string_in_python.06_fstring
+What is fstring in python.
+
+f-string a function in python this upgraded version of 'format. python -v 3.6'
+this f string
+"""
+
+name = "Rahul"
+amount = "200"
+
+print(f"Hay {name} nice to work here is your {amount}$ payment.\n Thank you of effort")
+
+print("\nProgram Finieshed.")
