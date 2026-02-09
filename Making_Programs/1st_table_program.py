9f26719f9c640848ce778c0a1851a262c9663bf6
imavinashex <avinashrf99-git@gmail.com>
2026-02-09 15:06:20 +0530

    Have some corecttion

--- Making_Programs/1st_table_program.py
+++ Making_Programs/1st_table_program.py
@@ -13,13 +13,13 @@ num7 =  8
 num8 =  9
 num9 =  10
 # i am using range method to print multiplication table from 1 to 11.
-# then in multiply with each number from 2 to 10 and i add essy to understand string like x , = and escape squence charector for batter view in terminal.
+# then in multiply with each number from 2 to 10 and i add easy to understand string like "x" , = and escape squence charector for batter view in terminal.
 
 for a in range(1, 11):
     print(num1,"X",a,"=",num1*a,"\t",num2,"X",a,"=",num2*a,"\t",num3,"X",a,"=",num3*a,"\t",num4,"X",a,"=",num4*a,"\t",num5,"X",a,"=",num5*a,"\t",num6,"X",a,"=",num6*a,"\t",num7,"X",a,"=",num7*a,"\t",num8,"X",a,"=",num8*a,"\t",num9,"X",a,"=",num9*a,"\n")
 
 
-print("Program Fineshed\n Thank you")
+print("Program Fineshed\n Thank you!")
 
 
 
