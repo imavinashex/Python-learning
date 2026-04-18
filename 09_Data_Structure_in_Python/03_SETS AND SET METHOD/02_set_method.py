750b5400de016df4fea5f9bf8e594e27b2308167
imavinashex <avinashrf99@gmail.com>
2026-04-18 17:50:38 +0530

    correcting some mistake in set_method

--- 09_Data_Structure_in_Python/03_SETS AND SET METHOD/02_set_method.py
+++ 09_Data_Structure_in_Python/03_SETS AND SET METHOD/02_set_method.py
@@ -7,8 +7,10 @@
 
 
 
-my_set ={1,2,3,4}
+my_set ={1,2,3,4,}
 
 my_set.add(37)
+my_set.add("Hello World!")
 my_set.remove(2)
-print(my_set)
+my_set.discard(56573)  # Note* If this number present in my_my set then this number will remove.
+print(my_set, type(my_set))
