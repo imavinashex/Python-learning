9eed61ea02d74b8e7abc19b9ac5204233ef3813f
imavinashex <avinashrf99@gmail.com>
2026-05-02 10:23:52 +0530

    change in variable name and Data's

--- 09_Data_Structure_in_Python/04_Dictionaries_and_Dictionary_Methods/01_creating_a_Dictionary.py
+++ 09_Data_Structure_in_Python/04_Dictionaries_and_Dictionary_Methods/01_creating_a_Dictionary.py
@@ -4,7 +4,7 @@ Dictionaries store key-value pairs and allow fast lookups
 In this example i see how to create dictionary ,usre curlie bracat {}, collon *:* and comma *,*
 """
 
-student ={"name":"Alice","age":21,"grade":"A" ,"city":"New Delhi"}
+student ={"name":"Narandra Godi","age":69,"grade":"G" ,"city":"New Delhi"}
 
 
-print(student["city"],student["grade"],student["age"],student["name"])
+print(student["name"],student["grade"],student["age"],student["city"])
