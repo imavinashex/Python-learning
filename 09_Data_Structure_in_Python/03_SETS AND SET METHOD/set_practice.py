7bbdd4d58a04d2f113aba0378ce70ab3167b97bc
imavinashex <avinashrf99@gmail.com>
2026-05-02 10:21:33 +0530

    Learn set opration or practice set

---
+++ 09_Data_Structure_in_Python/03_SETS AND SET METHOD/set_practice.py
@@ -0,0 +1,11 @@
+# Hi Avi we are now practice set in python after long break Date 25-april-2026 Now we are going to make a program.
+
+# [Part 1]
+name = {"Andy", "Golu","Andy","SSR"}
+
+unique_name = set(name)
+
+print(unique_name) 
+
+# [Part 1] Description:
+# This program stores five name variables within a set. By using this method, the program automatically filters out duplicate entries and ensures that only unique name values are retained. 
