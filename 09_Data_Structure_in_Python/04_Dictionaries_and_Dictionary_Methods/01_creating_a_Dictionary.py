c9c25efa86d86059d987c80a458fb899535879bb
imavinashex <avinashrf99@gmail.com>
2026-05-07 08:53:21 +0530

    learned Data Structure Dictionarie create,methods

--- 09_Data_Structure_in_Python/04_Dictionaries_and_Dictionary_Methods/01_creating_a_Dictionary.py
+++ 09_Data_Structure_in_Python/04_Dictionaries_and_Dictionary_Methods/01_creating_a_Dictionary.py
@@ -8,3 +8,8 @@ student ={"name":"Narandra Godi","age":69,"grade":"G" ,"city":"New Delhi"}
 
 
 print(student["name"],student["grade"],student["age"],student["city"])
+# Accessing & Modifying Values:
+student["age"] = 26  #updating in Dictionaries key-value
+student["name"] = "Amit"
+
+print(student)
---
+++ 09_Data_Structure_in_Python/04_Dictionaries_and_Dictionary_Methods/02_Dictionary_common_method.py
@@ -0,0 +1,13 @@
+
+
+
+
+student ={"name":"Narandra Godi","age":69,"grade":"G","city":"New Delhi"}
+
+
+# some common methods
+
+print(student.keys())
+print(student.values())
+print(student.pop("age"))
+print(student.copy())
