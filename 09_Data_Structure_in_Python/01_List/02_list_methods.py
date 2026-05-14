64c0ab073d266653a511463a484357fa0f8332af
imavinashex <avinashrf99@gmail.com>
2026-05-14 11:24:50 +0530

    Re-upload

--- 09_Data_Structure_in_Python/01_List/02_list_methods.py
+++ 09_Data_Structure_in_Python/01_List/02_list_methods.py
@@ -1,8 +1,9 @@
-marks = [5, 2, 21, 5, 7]
+marks = [5, 2, 21, 5,]
 extra_marks = [53, 23, 32]
 
-print(marks)
+# print(marks)
 # marks.append(63) # This will change the original list
 # marks.pop()
-marks.extend(extra_marks)
+# marks.extend(extra_marks)
+marks.remove(21)
 print(marks)
--- 09_Data_Structure_in_Python/01_List/03_list_comprehension.py
+++ 09_Data_Structure_in_Python/01_List/03_list_comprehension.py
@@ -1,11 +1,11 @@
 # Create a list containing the table of 5
 
 
-# table = []
+table = []
 
-# for i in range(1, 11):
-#     table.append(5*i)
+for i in range(1, 11):
+    table.append(13*i)
 
-table = [5*i for i in range(1, 11)]
+# table = [5*i for i in range(1, 11)]
 
 print(table)
