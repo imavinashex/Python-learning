7759bafec6dda285f28026e1c67cddc35f547011
imavinashex <avinashrf99@gmail.com>
2026-05-14 13:11:33 +0530

    Crete dict

--- 09_Data_Structure_in_Python/04_Dictionaries_and_Dictionary_Methods/01_creating_a_Dictionary.py
+++ 09_Data_Structure_in_Python/04_Dictionaries_and_Dictionary_Methods/01_creating_a_Dictionary.py
@@ -4,7 +4,7 @@ Dictionaries store key-value pairs and allow fast lookups
 In this example i see how to create dictionary ,usre curlie bracat {}, collon *:* and comma *,*
 """
 
-student ={"name":"Narandra Godi","age":69,"grade":"G" ,"city":"New Delhi"}
+student ={"name":"Narandra Godi","age":69,"grade":"G" ,"city": "New Delhi"}
 
 
 print(student["name"],student["grade"],student["age"],student["city"])
