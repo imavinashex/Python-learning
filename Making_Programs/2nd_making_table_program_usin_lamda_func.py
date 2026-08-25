7bcb36dbd50785c66dc489f7be6c65ce7650a570
imavinashex <146335543+imavinashex@users.noreply.github.com>
2026-08-25 13:57:09 +0530

    I Made second table program using lemda function

---
+++ Making_Programs/2nd_making_table_program_usin_lamda_func.py
@@ -0,0 +1,28 @@
+number = (1,2,3,4,5,6,7,8,9,10)
+
+table_of_2 = list(map(lambda x: x * 2, number))
+table_of_3 = list(map(lambda x: x * 3, number))
+table_of_4 = list(map(lambda x: x * 4, number))
+table_of_5 = list(map(lambda x: x * 5, number))
+table_of_6 = list(map(lambda x: x * 6, number))
+table_of_7 = list(map(lambda x: x * 7, number))
+table_of_8 = list(map(lambda x: x * 8, number))
+table_of_9 = list(map(lambda x: x * 9, number))
+table_of_10 = list(map(lambda x: x * 10, number)) #just expand this u can create more table
+
+
+print(table_of_2,
+      "\n",table_of_3,
+      "\n",table_of_4,
+      "\n",table_of_5,
+      "\n",table_of_6,
+      "\n",table_of_7,
+      "\n",table_of_8,
+      "\n",table_of_9,
+      "\n",table_of_10 
+
+
+      
+
+      )
+#You cam expand more of
